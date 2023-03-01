19d47b33751997f2dfc977237aefccf04ae3748a
unknown <luciano.sarcinella@gmail.com>
2023-03-01 12:12:15 +0100

    api url

--- src/utils/apiSlice.js
+++ src/utils/apiSlice.js
@@ -1,8 +1,10 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+const apiUrl = process.env.REACT_APP_API_URL;
+
 
 export const apiSlice = createApi({
     reducerPath: 'api',
-    baseQuery: fetchBaseQuery({ baseUrl: '/' }),
+    baseQuery: fetchBaseQuery({ baseUrl: apiUrl }),
     endpoints: builder => ({
       getProductos: builder.query({
         query: pageId => `/productos/${pageId}`      
